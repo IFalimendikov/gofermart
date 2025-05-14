91cc6e4377eb1155c4d50e091b7fd2ddf6948cd5
IFalimendikov <golovanjot2@gmail.com>
2025-05-15 00:18:11 +0200

    test

--- internal/storage/get_balance.go
+++ internal/storage/get_balance.go
@@ -2,25 +2,35 @@ package storage
 
 import (
 	"context"
-	"fmt"
 	"gofermart/internal/models"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func (s *Storage) GetBalance(ctx context.Context, userID string) (models.Balance, error) {
-	var balance models.Balance
-	query := `SELECT login, current, withdrawn FROM balances WHERE login = $1`
-	row  := s.DB.QueryRowContext(ctx, query, userID)
-	
-	err := row.Scan(&balance.ID, &balance.Current, &balance.Withdrawn) 
-	if err != nil {
-		return balance, err
-	}
+    var balance models.Balance
+    var currentNull, withdrawnNull sql.NullFloat64
+    
+    query := `SELECT login, current, withdrawn FROM balances WHERE login = $1`
+    row := s.DB.QueryRowContext(ctx, query, userID)
+    
+    err := row.Scan(&balance.ID, &currentNull, &withdrawnNull)
+    if err != nil {
+        if err.Error() == "sql: no rows in result set" {
+            balance.ID = userID
+            balance.Current = 0
+            balance.Withdrawn = 0
+            return balance, nil
+        }
+        return balance, err
+    }
 
-	fmt.Println("balance is")
-	fmt.Println(balance)
+    if currentNull.Valid {
+        balance.Current = currentNull.Float64
+    }
+    if withdrawnNull.Valid {
+        balance.Withdrawn = withdrawnNull.Float64
+    }
 
-
-	return balance, nil
+    return balance, nil
 }
