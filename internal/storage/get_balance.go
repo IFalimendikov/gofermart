5433bc9b158481c9af23e64daae6c37b8f44cc16
IFalimendikov <golovanjot2@gmail.com>
2025-05-30 20:28:40 +0200

    fix balance

--- internal/storage/get_balance.go
+++ internal/storage/get_balance.go
@@ -10,17 +10,14 @@ import (
 func (s *Storage) GetBalance(ctx context.Context, login string) (models.Balance, error) {
 	var balance models.Balance
 
-	row, err := sq.Select("login", "current", "withdrawn").
+	row := sq.Select("login", "current", "withdrawn").
 		From("balances").
 		Where(sq.Eq{"login": login}).
 		RunWith(s.DB).
 		PlaceholderFormat(sq.Dollar).
-		QueryContext(ctx)
-	if err != nil {
-		return balance, err
-	}
+		QueryRowContext(ctx)
 
-	err = row.Scan(&balance.ID, &balance.Current, &balance.Withdrawn)
+	err := row.Scan(&balance.ID, &balance.Current, &balance.Withdrawn)
 	if err != nil {
 		if err.Error() == "sql: no rows in result set" {
 			balance.ID = login
@@ -28,9 +25,6 @@ func (s *Storage) GetBalance(ctx context.Context, login string) (models.Balance,
 			balance.Withdrawn = 0
 			return balance, nil
 		}
-		return balance, err
-	}
-	if err = row.Err(); err != nil {
 		return balance, err
 	}
 
